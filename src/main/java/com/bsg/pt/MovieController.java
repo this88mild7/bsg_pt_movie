de05a32eb1529e8c3d76962f82f164dddd705d2e
this88mild7 <this88mild7@gmail.com>
2013-07-25 15:56:30 +0900

    swf 테스트중

--- src/main/java/com/bsg/pt/MovieController.java
+++ src/main/java/com/bsg/pt/MovieController.java
@@ -15,12 +15,16 @@ public class MovieController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
 	
-	private String movieSrc = "/movie/ani_song07.mp4";
+	private String movieSrc = "/movie/source/ani_song07.mp4";
+	private String swfSrc = "/movie/source/test.swf";
+	private String flvSrc = "/movie/source/test.fla";
 	
 	@RequestMapping(value = "flowplayer.do")
 	public ModelAndView flowerplay() {
 		ModelAndView mav = new ModelAndView("flowplayer");
 		mav.addObject("movie", movieSrc);
+		mav.addObject("swfSrc", swfSrc);
+		mav.addObject("flvSrc", flvSrc);
 		return mav;
 	}
 	
@@ -30,6 +34,8 @@ public class MovieController {
 		
 		ModelAndView mav = new ModelAndView("jwplayer");
 		mav.addObject("movie", movieSrc);
+		mav.addObject("swfSrc", swfSrc);
+		mav.addObject("flvSrc", flvSrc);
 		return mav;
 	}
 	
